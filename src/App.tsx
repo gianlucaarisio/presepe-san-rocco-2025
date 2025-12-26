e485b034e53f5603a0fc6a7929f9ece8aee2251c
Gianluca Arisio <gianlucaarisio@gmail.com>
2025-12-26 14:00:18 +0100

    Add: Vercel Analytics to the application

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,19 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
 import Layout from "./components/Layout";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App() {
   return (
-    <Routes>
-      <Route element={<Layout />}>
-        <Route index path="/" element={<Home />} />
-        <Route path="*" element={<NotFound />} />
-      </Route>
-    </Routes>
+    <>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route index path="/" element={<Home />} />
+          <Route path="*" element={<NotFound />} />
+        </Route>
+      </Routes>
+
+      <Analytics />
+    </>
   );
 }
